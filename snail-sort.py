2769573e97eec5a7e7b665b6308045d4b26169bd
Abdul Arham <133810564+A-Arham@users.noreply.github.com>
2025-05-07 00:19:51 +0500

    Refactoring

--- snail-sort.py
+++ snail-sort.py
@@ -6,4 +6,14 @@
 # snail(array) #=> [1,2,3,6,9,8,7,4,5]
 
 
+import numpy as np
+def snail(snail_map):
+    l=np.flatten(snail_map)
 
+
+array1 = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+
+
+print(snail(array1))

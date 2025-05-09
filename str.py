1ffbb9c4ab5764cc41e7d69a997f93d18e3cc195
Abdul Arham <133810564+A-Arham@users.noreply.github.com>
2025-05-10 00:49:24 +0500

    basic strings practice

---
+++ str.py
@@ -0,0 +1,26 @@
+# Given mission name
+mission_name = "Sort"
+
+# TODO: Print the first and the last character of the mission name
+
+# TODO: The mission needs a minor update. We aim to change its first letter to 'P' and the last letter to `k`, obtaining the word "Pork".
+# Remember, strings in Python are immutable, so you cannot alter them directly.
+
+# TODO: Print the updated mission name
+
+# print(mission_name[0:1])
+# print(mission_name[-1])
+
+
+
+
+print(mission_name[0:1])
+print(mission_name[-1])
+
+
+mission_name=mission_name.replace("S","F")
+mission_name=mission_name.replace("t","k")
+print(mission_name)
+
+
+
